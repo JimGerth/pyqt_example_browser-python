20a2a2087efc9dc129ee21988ebab6e5342450f9
JimGerth <jim.frederic.gerth@gmail.com>
2019-08-07 13:00:51 +0200

    added web view to application window

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import QUrl
 
 
 app = QApplication([])
+web_view = QWebEngineView()
+web_view.setUrl(QUrl('https://www.google.com'))
+web_view.show()
 app.exec_()
