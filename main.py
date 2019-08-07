aa63a9470c57d9ebcadb31c8ff7b2341f9f43f1a
JimGerth <jim.frederic.gerth@gmail.com>
2019-08-07 13:05:04 +0200

    added text field under browser window

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ from PyQt5.QtCore import QUrl
 
 
 app = QApplication([])
+
 web_view = QWebEngineView()
 web_view.setUrl(QUrl('https://www.google.com'))
-web_view.show()
+
+window = QWidget()
+
+layout = QVBoxLayout()
+layout.addWidget(web_view)
+layout.addWidget(QTextEdit())
+
+window.setLayout(layout)
+window.show()
+
 app.exec_()
