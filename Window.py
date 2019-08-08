debdfe81367de403b3483be967b43725cbaea023
JimGerth <jim.frederic.gerth@gmail.com>
2019-08-08 11:12:31 +0200

    added printing menu button...

--- Window.py
+++ Window.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QAction
 from Browser import Browser
 
 
@@ -7,3 +8,18 @@ class Window(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setCentralWidget(Browser())
+        self._setup_actions()
+        self._setup_menu_bar()
+
+    def _setup_actions(self):
+        self._print_action = QAction('print')
+        self._print_action.setShortcut('Ctrl+P')
+        self._print_action.triggered.connect(self._print)
+
+    def _setup_menu_bar(self):
+        self._menu_bar = self.menuBar()
+        self._print_menu = self._menu_bar.addMenu('print')
+        self._print_menu.addAction(self._print_action)
+
+    def _print(self):
+        print('printing...')
