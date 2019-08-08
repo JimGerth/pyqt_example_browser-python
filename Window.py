de4ab3cbf1bf8c83c0ceb87e11f91cdf2e7a1ddf
JimGerth <jim.frederic.gerth@gmail.com>
2019-08-08 11:15:24 +0200

    changed name of print menu to file menu and capitalized file and print

--- Window.py
+++ Window.py
@@ -12,14 +12,14 @@ class Window(QMainWindow):
         self._setup_menu_bar()
 
     def _setup_actions(self):
-        self._print_action = QAction('print')
+        self._print_action = QAction('Print')
         self._print_action.setShortcut('Ctrl+P')
         self._print_action.triggered.connect(self._print)
 
     def _setup_menu_bar(self):
         self._menu_bar = self.menuBar()
-        self._print_menu = self._menu_bar.addMenu('print')
-        self._print_menu.addAction(self._print_action)
+        self._file_menu = self._menu_bar.addMenu('File')
+        self._file_menu.addAction(self._print_action)
 
     def _print(self):
         print('printing...')
